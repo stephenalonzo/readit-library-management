9ed44ad60a318c74e1900442c03eb012008b5983
Stephen Alonzo <stephena0228@gmail.com>
2023-10-17 19:57:48 -0700

    Added a template function to appViews.php

--- app-views.php
+++ app-views.php
@@ -3,6 +3,66 @@
 class appViews
 {
 
+    public function appTemplate($component)
+    {
+
+        $template =
+        '<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Home | ReadIT Library Management System</title>
+            <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css" rel="stylesheet" type="text/css" />
+            <link rel="stylesheet" href="./dist/output.css">
+        </head>
+        <body class="bg-gray-100">
+            <section class="px-4 py-6">
+                <div class="container mx-auto">
+                    <div class="flex flex-row space-x-8 w-full">
+                        <div class="col-span-2 w-1/3 rounded-sm">
+                            <ul>
+                                <li class="pl-4 text-white py-4 bg-stone-800">
+                                    <a href="./">Home</a>
+                                </li>
+                                <hr>
+                                <li class="pl-4 text-white py-4 bg-stone-800">
+                                    <a href="#">Students for Approval</a>
+                                </li>
+                                <hr>
+                                <li class="pl-4 text-white py-4 bg-stone-800">
+                                    <a href="./manage-students.php">Manage Students</a>
+                                </li>
+                                <hr>
+                                <li class="pl-4 text-white py-4 bg-stone-800">
+                                    <a href="./manage-library.php">Manage Library</a>
+                                </li>
+                                <hr>
+                                <li class="pl-4 text-white py-4 bg-stone-800">
+                                    <a href="./add-book-form.php">Add Books</a>
+                                </li>
+                                <hr>
+                                <li class="pl-4 text-white py-4 bg-stone-800">
+                                    <a href="./issue-return-book.php">Issue/Return Books</a>
+                                </li>
+                                <hr>
+                                <li class="pl-4 text-white py-4 bg-stone-800">
+                                    <a href="./currently-ssued-books.php">View Currently Issued Books</a>
+                                </li>
+                                <hr>
+                            </ul>
+                        </div>
+                        '.$component.'
+                    </div>
+                </div>
+            </section>
+        </body>
+        </html>';
+
+        return $template;
+
+    }
+
     public function viewStudents($results)
     {
 
@@ -26,7 +86,7 @@ class appViews
 
     }
 
-    public function getNonIssuedBooks($results)
+    public function viewNonIssuedBooks($results)
     {
 
         foreach ($results as $row)
@@ -47,7 +107,7 @@ class appViews
 
     }
 
-    public function getIssuedBooks($results)
+    public function viewIssuedBooks($results)
     {
 
         foreach ($results as $row)
@@ -65,7 +125,31 @@ class appViews
 
         }
 
+    }
 
+    public function viewBooks($results)
+    {
+
+        foreach ($results as $row)
+        {
+
+            $book_row = 
+            '<tr>
+                <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-center">
+                    '.$row['id'].'
+                </td>
+                <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-center">
+                    '.$row['book_title'].'
+                </td>
+                <td class="whitespace-nowrap px-4 py-2 text-gray-700 text-center">'.$row['author_name'].'</td>
+                <td class="whitespace-nowrap px-4 py-2 text-gray-700 text-center">'.$row['book_category'].'</td>
+                <td class="whitespace-nowrap px-4 py-2 text-gray-700 text-center">'.$row['current_issue'].'</td>
+            </tr>';
+            
+        }
+        
+        return $book_row;
+        
     }
 
 }
